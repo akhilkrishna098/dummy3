354d3a4f0bdf980ced9b39b0c4166b0cd424a69b
akhil_krishna <aakhilkrishna098@gmail.com>
2023-11-01 14:06:25 +0100

    init push

---
+++ src/main/java/com/example/dummy3/Dummy3Application.java
@@ -0,0 +1,13 @@
+package com.example.dummy3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dummy3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dummy3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dummy3/Dummy3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dummy3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dummy3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
